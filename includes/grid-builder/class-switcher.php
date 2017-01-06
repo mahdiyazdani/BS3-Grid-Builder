50045c19531f12c785a002882d8da47caa1b0c05
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-01-07 02:21:48 +0400

    Removed grid builder switcher method

--- includes/grid-builder/class-switcher.php
+++ includes/grid-builder/class-switcher.php
@@ -4,7 +4,7 @@
  *
  * @author      Mahdi Yazdani
  * @package     BS3 Grid Builder
- * @since       1.0
+ * @since       1.0.3
  */
 namespace bs3_grid_builder\builder;
 use bs3_grid_builder\BS3_Grid_Builder_Functions as Bs3;
@@ -29,8 +29,6 @@ class BS3_Grid_Builder_Switcher{
 		add_action( 'admin_head', array( $this, 'html_class_script' ) );
 		// Add Editor/BS3 Grid Builder Tab
 		add_action( 'edit_form_after_title', array( $this, 'editor_toggle' ) );
-		// Save BS3_Grid_Builder_Switcher Preference
-		add_action( 'save_post', array( $this, 'save' ), 9, 2 );
 		// Scripts
 		add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ), 99 );
 	}
@@ -72,33 +70,6 @@ class BS3_Grid_Builder_Switcher{
 				<?php do_action( 'bs3_grid_builder_switcher_nav', $post ); ?>
 			</h1>
 			<?php
-		endif;
-	}
-
-	// Save grid builder switcher prefrences
-	public function save( $post_id, $post ){
-		$request = stripslashes_deep( $_POST );
-		if ( ! isset( $request['bs3_grid_builder_switcher_nonce'] ) || ! wp_verify_nonce( $request['bs3_grid_builder_switcher_nonce'], __FILE__ ) ):
-			return false;
-		endif;
-		if( defined('DOING_AUTOSAVE' ) && DOING_AUTOSAVE ):
-			return false;
-		endif;
-		$post_type = get_post_type_object( $post->post_type );
-		if ( !current_user_can( $post_type->cap->edit_post, $post_id ) ):
-			return false;
-		endif;
-
-		// Save BS3_Grid_Builder_GBF Tab
-		if( isset( $request['_bs3_grid_builder_active'] ) ):
-			$new_data = $request['_bs3_grid_builder_active'] ? 1 : 0;
-			if( $new_data ):
-				update_post_meta( $post_id, '_bs3_grid_builder_active', 1 );
-			else:
-				delete_post_meta( $post_id, '_bs3_grid_builder_active' );
-			endif;
-		else:
-			delete_post_meta( $post_id, '_bs3_grid_builder_active' );
 		endif;
 	}
 
